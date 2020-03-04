4e8b504bb379a87c68f18b841b88a2deb6eb12af
Shiva <shiva.el@gmail.com>
2020-03-03 21:27:34 -0500

    add ability to preprocessing to read csv files from zip, since csv files are to big to push to github

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,13 @@
+import zipfile
+
 import pandas as pd
 
 from src import config
 
 
 def load_data():
-    df_users = pd.read_csv(config.DATA_PATH + "\\train_users_2.csv")
-    df_sessions = pd.read_csv(config.DATA_PATH + "\\sessions.csv")
+    zf = zipfile.ZipFile(config.DATA_PATH + "\\new-user-bookings.zip")
+    df_users = pd.read_csv(zf.open("train_users_2.csv"))
+    df_sessions = pd.read_csv(zf.open("sessions.csv"))
 
     return df_users, df_sessions
